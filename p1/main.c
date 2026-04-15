dcac5c559637bc310cacaf2db53a867a10b3b4f5
2025050210 <yesjs120716@gmail.com>
2026-04-15 15:54:17 +0900

    Update main.c

---
+++ p1/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int answer, guess;
+    int count=0;
+    
+    scanf("%d\n", &answer);
+    
+    do{
+        scanf("%d\n", &guess);
+        count++;
+        
+        if(answer<guess){
+            printf("%d>?", guess);
+        }
+        else if(answer>guess){
+            printf("%d<?", guess);
+            
+        }
+        else{
+            printf("%d==?\n", guess);
+        }
+    }while (guess!=answer);
+    
+    printf("%d\n", count);
+
+    return 0;
+}
